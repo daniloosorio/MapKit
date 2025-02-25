7e58d55744c7ca8bfe60342ff7b52630f317439c
Danilo Osorio <odanilo38@gmail.com>
2025-02-24 21:41:14 -0500

    Initial Commit

---
+++ MapKitApp/MapKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitAppApp.swift
+//  MapKitApp
+//
+//  Created by Danilo Osorio on 24/02/25.
+//
+
+import SwiftUI
+
+@main
+struct MapKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MapKitAppTests/MapKitAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitAppTests.swift
+//  MapKitAppTests
+//
+//  Created by Danilo Osorio on 24/02/25.
+//
+
+import Testing
+@testable import MapKitApp
+
+struct MapKitAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
